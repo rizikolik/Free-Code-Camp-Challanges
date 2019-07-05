b03bc51da060630027172907d30d23210fd639d2
Yunusture <46383770+rizikolik@users.noreply.github.com>
2019-07-05 13:08:30 +0300

    Update and rename MongoWorks to App.js

---
+++ App.js
@@ -0,0 +1,76 @@
+var mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URI);
+
+/*====================================
+CREATE A SCHEMA AND A MODEL 
+====================================*/
+var PersonSchema=new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods :{
+    type: Array,
+  
+  }
+  
+});
+var Person=mongoose.model("Person",PersonSchema);
+
+/*===============================================
+CREATE A NEW INSTANCE AND SAVE IT WİTH A CALLBACK
+================================================*/
+var createAndSavePerson = function(done) {
+  const person=new Person({
+  name:"dfdfdf",
+  age:12,
+  favoriteFoods:["njfdbfdj","fdfdf"]
+});
+person.save((err,data)=>{
+  if(err){
+ return  done(err);
+  }else{
+     return done(null, data);
+  }
+})};
+/*============================================
+CREATE MANY WITH AN ARRAY USING MODEL.CREATE()
+=============================================*/
+var createManyPeople = function(arrayOfPeople, done) {
+    Person.create(arrayOfPeople, function (err, data) {
+      if (err) {
+        done(err);
+      }
+    done(null, data);
+    });
+    
+};
+
+/*==========================================================
+FIND ALL PEOPLE MATHES THE GIVEN PARAMETER WITH `Model.find()
+===========================================================*/
+var findPeopleByName = function(personName, done) {
+  Person.find({name:personName},(err,data)=>{
+             if (err) {
+        done(err);
+      }
+    done(null, data);  
+              })
+};
+
+/*=======================================================================
+FIND JUST ONE PERSON THAT HAS A "favoriteFoods:" KEYS  MATHCHES THE QUERY
+========================================================================*/
+var findOneByFood = function(food, done) {
+Person.findOne({favoriteFoods:food},(err,data)=>{
+  if(err){
+    return done(err);
+  }
+ return  done(null,data)
+})
+  
+  
+};
