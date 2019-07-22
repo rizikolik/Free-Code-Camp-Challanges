5dbc8d08cee6fdaf356ddb4447e933a950e94484
rizikolik <ynsture@gmail.com>
2019-07-22 13:38:59 +0300

    promise all with await added

--- Promises/async-await/app.js
+++ Promises/async-await/app.js
@@ -23,3 +23,17 @@ async function hostDinnerParty() {
  }
  
  hostDinnerParty();
+
+ //Promise all with await //
+ let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
+
+async function serveDinnerAgain(){
+  let foodArray=await Promise.all([steamBroccoli(), cookRice(), bakeChicken(),cookBeans()]);
+let vegetable = foodArray[0];
+let starch =  foodArray[1];
+let protein =  foodArray[2];
+let side =  foodArray[3];
+
+console.log(`Dinner is served. We're having ${vegetable}, ${starch}, ${protein}, and ${side}.`);
+}
+serveDinnerAgain()
